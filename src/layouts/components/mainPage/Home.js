09e3d73e9ef52d0f1069d597ab76b4cb6756c5c2
hoang_anh <ledinhhoanganh10@gmail.com>
2024-10-09 22:52:58 +0700

    next step

--- src/layouts/components/mainPage/Home.js
+++ src/layouts/components/mainPage/Home.js
@@ -6,16 +6,27 @@ const cx = classNames.bind(styles);
 
 export default function Home() {
    return (
-      <div className={cx('wrapper')}>
-         <h1 className={cx('header')}>Virtual Tours Made Simple</h1>
-         <p className={cx('des')}>Create your future life and look after your family</p>
-         <button className={cx('home-button')}>Dashboard</button>
-         <button className={cx('home-button')}>Dashboard</button>
-         <div>
-            <img className={cx('img')} src={images.natural} alt="logo" />
-            <img className={cx('img')} src={images.natural} alt="logo" />
-            <img className={cx('img')} src={images.natural} alt="logo" />
+      <>
+         <div className={cx('wrapper')}>
+            <h1 className={cx('header')}>Virtual Tours Made Simple</h1>
+            <p className={cx('des')}>If you have a place to go when being tired, it is your home</p>
+            <button className={cx('home-button')}>TRY FOR FREE </button>
+            <button className={cx('home-button')}>MORE INFO</button>{' '}
+            <div className={cx('img-content')}>
+               <div className={cx('img-container')}>
+                  <p className={cx('img-title')}>House</p>
+                  <img className={cx('img')} src={images.natural} alt="logo" />
+               </div>
+               <div className={cx('img-container')}>
+                  <p className={cx('img-title')}>House</p>
+                  <img className={cx('img')} src={images.natural} alt="logo" />
+               </div>
+               <div className={cx('img-container')}>
+                  <p className={cx('img-title')}>House</p>
+                  <img className={cx('img')} src={images.natural} alt="logo" />
+               </div>
+            </div>
          </div>
-      </div>
+      </>
    );
 }
